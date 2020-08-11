17c3ce24139a96449dffa912a89b1c66ddda493f
Seonghoi Lee <mighty1231@gmail.com>
2020-08-11 16:17:49 +0900

    Update emulator path

--- androidkit/androidkit/emulator.py
+++ androidkit/androidkit/emulator.py
@@ -112,8 +112,10 @@ def emulator_run_and_wait(avd_name, serial=None, snapshot=None, wipe_data=False,
                 'partition_size_in_mb should be integer, given {}'.format(partition_size_in_mb)
         emulator_cmd.append('-partition-size {}'.format(partition_size_in_mb))
 
+    print("Emulator command:", ' '.join(emulator_cmd))
+
     proc = subprocess.Popen(' '.join(emulator_cmd), stdout=w_fd, stderr=w_fd, shell=True,
-        cwd = os.path.join(getConfig('SDK_PATH'), 'tools'))
+        cwd = os.path.join(getConfig('SDK_PATH'), 'emulator'))
     os.close(w_fd)
 
     emulator_wait_for_boot(avd_name, r_fd, serial=serial)
